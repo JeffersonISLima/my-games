9a9338e459aec72a121d499f84cc88a81c0aac04
Jefferson <jeffersoninacio@hotmail.com>
2021-05-13 22:35:27 -0300

    first commit :tada:

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header"> Hello word Home</header>
+    </div>
+  );
+}
+
+export default Home;
